de0eb40aa745817b040f80bf597a965c0f3daff4
limhaekyu <limk9500@gmail.com>
2022-11-22 12:39:54 +0900

    Initial commit

---
+++ src/main/java/com/example/good_match/GoodMatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.good_match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/good_match/GoodMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.good_match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

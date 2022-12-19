f2c034a8c2b465631125e5d95cf8641eced7ac39
limhaekyu <limk9500@gmail.com>
2022-12-19 20:16:57 +0900

    style: controller swagger 설명 추가

--- src/main/java/com/example/good_match/domain/member/controller/MemberController.java
+++ src/main/java/com/example/good_match/domain/member/controller/MemberController.java
@@ -7,6 +7,7 @@ import com.example.good_match.domain.member.dto.request.SignUpRequestDto;
 import com.example.good_match.domain.member.dto.response.FindIdResponseDto;
 import com.example.good_match.domain.member.service.MemberService;
 import com.example.good_match.global.response.ApiResponseDto;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,21 +18,25 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    @ApiOperation(value = "회원가입")
     @PostMapping("/sign-up")
     public ApiResponseDto signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         return memberService.signUpMember(signUpRequestDto);
     }
 
+    @ApiOperation(value = "로그인")
     @PostMapping("/login")
     public ApiResponseDto login(@RequestBody LoginRequestDto loginRequestDto) {
         return memberService.login(loginRequestDto);
     }
 
+    @ApiOperation(value = "엑세스 토큰 재발급")
     @PostMapping("/reissue")
     public ApiResponseDto reissue(@RequestBody ReissueRequestDto reissueRequestDto) {
         return memberService.reissue(reissueRequestDto);
     }
 
+    @ApiOperation(value = "아이디 찾기")
     @PostMapping("/find-id")
     public ApiResponseDto<FindIdResponseDto> findId(@RequestBody FindIdRequestDto findIdRequestDto){
         return memberService.findId(findIdRequestDto);

6edc6fa162f2f25d6b18908153666679a0715921
limhaekyu <limk9500@gmail.com>
2023-02-14 00:31:00 +0900

    fix: [Board] 함수명 변경 후 controller 미적용 수정

--- src/main/java/com/example/good_match/domain/board/controller/BoardController.java
+++ src/main/java/com/example/good_match/domain/board/controller/BoardController.java
@@ -21,26 +21,26 @@ public class BoardController {
     @ApiOperation(value = "[게시글] 게시글 등록")
     @PostMapping("")
     public ApiResponseDto addGame(@AuthenticationPrincipal User user, @RequestBody AddBoardRequestDto addBoardRequestDto) {
-        return boardService.addGame(addBoardRequestDto, user);
+        return boardService.addBoard(addBoardRequestDto, user);
     }
 
     @ApiOperation(value = "[게시글] 게시글 상세조회")
     @GetMapping("/{id}")
     public ApiResponseDto selectGameDetail(@PathVariable Long id){
-        return boardService.selectGameDetail(id);
+        return boardService.selectBoardDetail(id);
     }
 
     @ApiOperation(value = "[게시글] 게시글 삭제")
     @DeleteMapping("/{id}")
     public ApiResponseDto deleteGame(@PathVariable Long id, @AuthenticationPrincipal User user) {
-        return boardService.deleteGame(id, user);
+        return boardService.deleteBoard(id, user);
     }
 
     // 게시글 수정
     @ApiOperation(value = "[게시글] 게시글 수정")
     @PutMapping("/{id}")
     public ApiResponseDto updateGame(@PathVariable Long id, @RequestBody UpdateBoardRequestDto updateBoardRequestDto, @AuthenticationPrincipal User user) {
-        return boardService.updateGame(id, updateBoardRequestDto, user);
+        return boardService.updateBoard(id, updateBoardRequestDto, user);
     }
 
 }

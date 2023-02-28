a78d7be5b821d05b5ef71584cc7bc573cbe9f2ce
limhaekyu <limk9500@gmail.com>
2023-02-28 22:54:57 +0900

    fix: [Auth] Session Listener 제거
    - Ec2 서버에서 발생한 에러가 Nginx로 인해 yml파일의 데이터를 못 읽어온다고 생각해서 Listener를 생성했지만 내장 톰캣 이외의 WAS에서 실행할 때 Listener를 설정해줘야 하기 때문에 제거

--- src/main/java/com/example/good_match/global/config/WebMvcConfig.java
+++ src/main/java/com/example/good_match/global/config/WebMvcConfig.java
@@ -3,15 +3,12 @@ package com.example.good_match.global.config;
 import com.example.good_match.global.interceptor.AuthInterceptor;
 import com.example.good_match.global.interceptor.LoginInterceptor;
 import com.example.good_match.global.util.CurrentMemberIdResolver;
-import com.example.good_match.global.util.SessionListener;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.http.HttpSessionListener;
 import java.util.List;
 
 @Configuration
@@ -30,10 +27,5 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentMemberIdResolver);
-    }
-
-    @Bean
-    public HttpSessionListener httpSessionListener() {
-        return new SessionListener();
     }
 }

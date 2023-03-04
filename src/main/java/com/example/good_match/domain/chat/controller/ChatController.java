9c306fa507b8fb575e4e3d0a434e392355f4aeec
limhaekyu <limk9500@gmail.com>
2023-03-04 12:51:03 +0900

    feat: [Chat] Web Socket을 이용해 단일 채팅 기능 개발
    - 현재 하나의 web socket에 연결된 클라이언트끼리만 메시지 가능

---
+++ src/main/java/com/example/good_match/domain/chat/controller/ChatController.java
@@ -0,0 +1,23 @@
+package com.example.good_match.domain.chat.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class ChatController {
+
+    @GetMapping("/chat")
+    public String chatGet(){
+
+        log.info("@ChatController, chat GET()");
+
+        return "chat";
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/good_match/global/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.example.good_match.global.config;
+
+import com.example.good_match.global.handler.ChatWebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}

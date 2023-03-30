6ccd8e538dfee53739d9e65faf5466a230be139f
limhaekyu <limk9500@gmail.com>
2023-03-30 14:02:59 +0900

    refactor: [게시글] Return type 변경, 유닛테스트 코드 작성, 객체 변수 숨기기위한 리팩터링 작업

--- src/main/java/com/example/good_match/domain/post/controller/PostController.java
+++ src/main/java/com/example/good_match/domain/post/controller/PostController.java
@@ -2,6 +2,7 @@ package com.example.good_match.domain.post.controller;
 
 import com.example.good_match.domain.post.dto.request.AddPostRequestDto;
 import com.example.good_match.domain.post.dto.request.UpdatePostRequestDto;
+import com.example.good_match.domain.post.dto.response.SelectPostDetailResponseDto;
 import com.example.good_match.domain.post.service.PostService;
 import com.example.good_match.global.annotation.CurrentMemberId;
 import com.example.good_match.global.annotation.LoginRequired;
@@ -9,6 +10,7 @@ import com.example.good_match.global.response.ApiResponseDto;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,29 +24,31 @@ public class PostController {
     @LoginRequired
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public ApiResponseDto insertPost(@CurrentMemberId Long memberId, @RequestBody AddPostRequestDto addPostRequestDto) {
-        return postService.insertPost(addPostRequestDto, memberId);
+    public void insertPost(@CurrentMemberId Long memberId, @RequestBody AddPostRequestDto addPostRequestDto) {
+        postService.insertPost(addPostRequestDto, memberId);
     }
 
     @ApiOperation(value = "[게시글] 게시글 상세조회")
     @LoginRequired
     @GetMapping("/{id}")
-    public ApiResponseDto selectPostDetail(@PathVariable Long id){
-        return postService.selectPostDetail(id);
+    public ResponseEntity<SelectPostDetailResponseDto> selectPostDetail(@PathVariable Long id){
+        return ResponseEntity.ok().body(postService.selectPostDetail(id));
     }
 
     @ApiOperation(value = "[게시글] 게시글 삭제")
     @LoginRequired
+    @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
-    public ApiResponseDto deletePost(@PathVariable Long id, @CurrentMemberId Long memberId) {
-        return postService.deletePost(id, memberId);
+    public void deletePost(@PathVariable Long id, @CurrentMemberId Long memberId) {
+        postService.deletePost(id, memberId);
     }
 
     @ApiOperation(value = "[게시글] 게시글 수정")
     @LoginRequired
+    @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
-    public ApiResponseDto updatePost(@PathVariable Long id, @RequestBody UpdatePostRequestDto updatePostRequestDto, @CurrentMemberId Long memberId) {
-        return postService.updatePost(id, updatePostRequestDto, memberId);
+    public void updatePost(@PathVariable Long id, @RequestBody UpdatePostRequestDto updatePostRequestDto, @CurrentMemberId Long memberId) {
+        postService.updatePost(id, updatePostRequestDto, memberId);
     }
 
 }
--- src/main/java/com/example/good_match/domain/post/service/PostServiceImpl.java
+++ src/main/java/com/example/good_match/domain/post/service/PostServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.good_match.domain.post.service;
 
 import com.example.good_match.domain.category.repository.CategoryRepository;
 import com.example.good_match.domain.category.repository.SubCategoryRepository;
+import com.example.good_match.domain.category.service.CategoryService;
+import com.example.good_match.domain.member.model.Authority;
 import com.example.good_match.domain.member.model.Member;
 import com.example.good_match.domain.member.service.MemberService;
 import com.example.good_match.domain.post.domain.Post;
@@ -15,31 +17,32 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.security.auth.message.AuthException;
+
 @Service
 @RequiredArgsConstructor
-public class PostServiceImpl implements PostService {
+public class PostServiceImpl implements PostService{
     private final PostRepository postRepository;
     private final MemberService memberService;
-    private final CategoryRepository categoryRepository;
-    private final SubCategoryRepository subCategoryRepository;
+
+    private final CategoryService categoryService;
 
     /*
         [매칭] 게임 게시글 등록
      */
     @Transactional
-    public ApiResponseDto insertPost(AddPostRequestDto addPostRequestDto, Long memberId) {
+    public void insertPost(AddPostRequestDto addPostRequestDto, Long memberId) {
         try{
             postRepository.save(Post.builder()
                     .title(addPostRequestDto.getTitle())
                     .contents(addPostRequestDto.getContents())
                     .states(addPostRequestDto.getStates())
                     .member(memberService.findMemberById(memberId))
-                    .category(categoryRepository.findById(addPostRequestDto.getCategoryId()).orElseThrow(()->new IllegalArgumentException("없는 카테고리입니다.")))
-                    .subCategory(subCategoryRepository.findById(addPostRequestDto.getSubCategoryId()).orElseThrow(()->new IllegalArgumentException("없는 서브 카테고리입니다.")))
+                    .category(categoryService.findCategoryById(addPostRequestDto.getCategoryId()))
+                    .subCategory(categoryService.findSubCategoryById(addPostRequestDto.getSubCategoryId()))
                     .build());
-            return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "게임 매칭 게시글 등록을 성공했습니다. ");
         } catch (Exception e){
-            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "게임 매칭 게시글 등록에 실패했습니다. " + e.getMessage());
+            throw new IllegalArgumentException("게시글 등록에 실패했습니다. " + e.getMessage());
         }
     }
 
@@ -47,23 +50,22 @@ public class PostServiceImpl implements PostService {
     /*
         [매칭] 게임 게시글 상세 조회
      */
-    @Transactional
-    public ApiResponseDto selectPostDetail(Long id) {
+    @Transactional(readOnly = true)
+    public SelectPostDetailResponseDto selectPostDetail(Long id) {
         try {
             Post post = findPostById(id);
 
-            return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "게임 매칭 게시글 상세 조회에 성공했습니다.",
-                    SelectPostDetailResponseDto.builder()
+            return SelectPostDetailResponseDto.builder()
                             .title(post.getTitle())
                             .contents(post.getContents())
                             .states(post.getStates())
                             .updatedAt(post.getUpdatedAt())
                             .memberId(post.getMember().getId())
                             .memberName(post.getMember().getName())
-                            .build()
-            );
+                            .build();
+
         } catch (Exception e) {
-            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "게임 매칭 게시글 상세 조회에 실패했습니다. " + e.getMessage());
+            throw new IllegalArgumentException("상세조회에 실패했습니다. " + e.getMessage());
         }
     }
 
@@ -73,18 +75,17 @@ public class PostServiceImpl implements PostService {
      */
 
     @Transactional
-    public ApiResponseDto deletePost(Long id, Long memberId) {
+    public void deletePost(Long id, Long memberId) {
         try {
             Post post = findPostById(id);
             Member member = memberService.findMemberById(memberId);
-            if (member == post.getMember() || member.getAuthority().equals("ROLE_ADMIN")) {
+            if (post.eqMember(member) || member.isAdmin()) {
                 postRepository.delete(post);
-                return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "게임 매칭 게시글을 삭제했습니다.");
             } else {
-                return ApiResponseDto.of(ResponseStatusCode.UNAUTHORIZED.getValue(), "해당 사용자의 게임 메칭 게시글이 아닙니다.");
+                throw new AuthException("해당 사용자의 게시글이 아닙니다.");
             }
         } catch (Exception e) {
-            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "게임 매칭 게시글을 삭제했습니다.. " + e.getMessage());
+            throw new IllegalArgumentException("게시글 삭제 실패!" + e.getMessage());
         }
     }
 
@@ -100,24 +101,19 @@ public class PostServiceImpl implements PostService {
     /*
         [매칭] 게임 게시글 수정
      */
-
     @Transactional
-    public ApiResponseDto updatePost(Long id, UpdatePostRequestDto updatePostRequestDto, Long memberId) {
+    public void updatePost(Long id, UpdatePostRequestDto updatePostRequestDto, Long memberId) {
         try {
             Post post = findPostById(id);
             Member member = memberService.findMemberById(memberId);
-            if (member == post.getMember() || member.getAuthority().equals("ROLE_ADMIN")) {
-
+            if (post.eqMember(member) || member.isAdmin()) {
                 post.updatePost(updatePostRequestDto.getTitle(), updatePostRequestDto.getContents(), updatePostRequestDto.getStates());
                 postRepository.save(post);
-
-                return ApiResponseDto.of(ResponseStatusCode.SUCCESS.getValue(), "게임 매칭 게시글 수정을 완료했습니다.");
             } else {
-                return ApiResponseDto.of(ResponseStatusCode.UNAUTHORIZED.getValue(), "수정 권한이 없습니다. ");
+                throw new AuthException("수정 권한이 없습니다.");
             }
-
         } catch (Exception e){
-            return ApiResponseDto.of(ResponseStatusCode.FAIL.getValue(), "게임 매칭 게시글 수정에 실패했습니다. " + e.getMessage());
+            throw new IllegalArgumentException("게시글 수정 실패! " +e.getMessage());
         }
     }
 
